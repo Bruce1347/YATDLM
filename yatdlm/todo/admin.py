f8bbb4c349e05bcfdc4303720564c74a35d7c82f
Adjou Kaci <adjou.kaci@gmail.com>
2017-12-18 23:07:50 +0100

    TodoList and Task update and registration into the admin interface

--- yatdlm/todo/admin.py
+++ yatdlm/todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TodoList
+from .models import Task
+
+# We register our models in order to be able to modify them in the admin interface
+admin.site.register(Task)
+admin.site.register(TodoList)
--- yatdlm/todo/models.py
+++ yatdlm/todo/models.py
@@ -4,20 +4,34 @@ from django.utils import timezone
 class TodoList(models.Model):
     # A short description of the todo list (mandatory)
     title = models.CharField(max_length=100, blank=False)
-    # The full description of the todo list 
-    description = models.CharField(max_length=600, blank=True)
+    # The full description of the todo list (optional)
+    description = models.TextField(max_length=600, blank=True)
     # We want the creation date of the list (mandatory)
     creation_date = models.DateTimeField(auto_now_add=True)
-    # The user may want the task completed upon a certain date
-    due_date = models.DateTimeField(auto_now_add=False, default=timezone.now, blank=True)
+    # The user may want the todo list completed upon a certain date (optional)
+    due_date = models.DateTimeField(auto_now_add=False, default=timezone.now, blank=True, null=True)
+
+    # Identify the todolist with its title
+    def __str__(self):
+        return self.title
 
 class Task(models.Model):
     # The primary key to the list that contains this task
     parent_list = models.ForeignKey('TodoList', on_delete=models.CASCADE)
     # The primary key to the parent task, since a task can have subtasks, this is a recursive relationship
-    parent_task = models.ForeignKey('self', on_delete=models.CASCADE)
+    parent_task = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    # Creation date (mandatory, automatically created and inalterable)    
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    # The user may want to complete the task within a certain time (optional)
+    due_date = models.DateTimeField(auto_now=False, default=timezone.now, null=True, blank=True)
 
     # A short description of the task (mandatory)
     title = models.CharField(max_length=100, blank=False)
-    # Its full description
-    description = models.CharField(max_length=300, blank=True)
+    # Its full description (optional)
+    description = models.TextField(max_length=300, blank=True)
+
+    # Identify the task with its title
+    def __str__(self):
+        return self.title
