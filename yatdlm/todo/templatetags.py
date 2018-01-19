47c5ecbaf5e8627df5d56241f78d743bb5939e7e
Kaci Adjou <adjou.kaci@gmail.com>
2018-01-19 22:23:54 +0100

    Task #36 : Add URL parsing inside comments display through a custom filter

--- yatdlm/todo/templatetags.py
+++
@@ -1,6 +0,0 @@
-from django.template.defaulttags import register
-
-# Custom filter in order to use dictionaries inside django templates
-@register.filter
-def get_value(dict, key):
-    return dict.get(key)
---
+++ yatdlm/todo/templatetags/tags.py
@@ -0,0 +1,25 @@
+from django.template.defaulttags import register
+from django.utils.safestring import mark_safe
+from django.template.defaultfilters import stringfilter
+
+import re
+
+# Custom filter in order to use dictionaries inside django templates
+@register.filter
+def get_value(dict, key):
+    return dict.get(key)
+
+# Custom filter that inserts hrefs into the given string then returns it
+@register.filter
+@stringfilter
+def display_urls(desc):
+    # Regex that matches URLs
+    url_regex = '(https?:\/\/[a-zA-Z0-9_-]+\.[a-zA-Z0-9]+[a-zA-Z0-9-_&#\/]+)'
+    urls_substrings = re.findall(url_regex, desc)
+
+    for url in urls_substrings:
+        href = '<a href="{}">{}</a>'.format(url, url)
+        desc = desc.replace(url, href)
+
+    # We need to mark as safe the returned string in order to avoid string escaping
+    return mark_safe(desc)
