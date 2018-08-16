b9b0f53342565bb1613d62e34f0ad038f928211d
Kaci Adjou <kaci.adjou@epita.fr>
2018-08-16 11:11:58 +0200

    Task #63: Add url parsing inside task detail page, add safeguard for recursion for URL parsing.

--- yatdlm/todo/templatetags/tags.py
+++ yatdlm/todo/templatetags/tags.py
@@ -15,11 +15,21 @@ def get_value(dict, key):
 def display_urls(desc):
     # Regex that matches URLs
     url_regex = '(https?:\/\/[a-zA-Z0-9_-]+\.[a-zA-Z0-9\/]+[a-zA-Z0-9-_&#\/\.]+)'
-    urls_substrings = re.findall(url_regex, desc)
+
+    # The set is used here to manage a stupid edge case when we have multiple
+    # occurrences of a substring.
+    # The trick is to have unique substrings in order to avoid recursion when
+    # we are using str.replace().
+    urls_substrings = set(re.findall(url_regex, desc))
 
     for url in urls_substrings:
-        href = '<a href="{}">{}</a>'.format(url, url)
-        desc = desc.replace(url, href)
+        href = '<a href="{0}">{0}</a>'.format(url)
+        # Second part of the stupid edge case : we have an intermediate step
+        # which consists of replace() the url by the string 'REPLACEME'.
+        # Thanks to this we avoid recursion and we can substitute 'REPLACEME'
+        # by hrefs containing `url`
+        desc = desc.replace(url, 'REPLACEME')
+        desc = desc.replace('REPLACEME', href)
 
     # We need to mark as safe the returned string in order to avoid string escaping
     return mark_safe(desc)
