e2ec9d03d669361ada776a8c15a2cb44f5f51c6c
Kaci Adjou <adjou.kaci@gmail.com>
2020-02-03 23:19:14 +0100

    #106: Add deletion unit tests
    This commit adds unit tests for the todolist deletion view in order to
    have some tests to rely on  in the future to be sure to not introduce
    breaking changes.

---
+++ yatdlm/todo/tests/test_todolist.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import models as auth_models
+from todo.models import TodoList
+
+class TodoListTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = auth_models.User.objects.create_user('test', password='1234')
+        cls.other_user = auth_models.User.objects.create_user('test2', password='1234')
+        cls.list_ = TodoList(owner=cls.user)
+        cls.list_.save()
+
+    def test_delete_list(self):
+        list_ = TodoList(owner=self.user)
+        list_.save()
+        self.client.login(username='test', password='1234')
+        response = self.client.delete('/todo/lists/delete/{}'.format(list_.id))
+        self.assertEqual(response.status_code, 204)
+        with self.assertRaises(TodoList.DoesNotExist):
+            TodoList.objects.get(id=list_.id)
+
+    def test_delete_list_wrong_user(self):
+        list_ = TodoList(owner=self.other_user)
+        list_.save()
+        self.client.login(username='test', password='1234')
+        response = self.client.delete('/todo/lists/delete/{}'.format(list_.id))
+        self.assertEqual(response.status_code, 403)
+        self.assertIsNotNone(TodoList.objects.get(id=list_.id))
+
+    def test_delete_list_wrong_verb(self):
+        self.client.login(username='test', password='1234')
+        response = self.client.post('/todo/lists/delete/{}'.format(self.list_.id))
+        # Expect a Method not allowed
+        self.assertEqual(response.status_code, 405)
+        self.assertIsNotNone(TodoList.objects.get(id=self.list_.id))
