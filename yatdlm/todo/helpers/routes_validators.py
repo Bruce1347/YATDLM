f157bdd30f06d7c76aace9e9c38b2e85a2547e89
Kaci Adjou <adjou.kaci@gmail.com>
2020-02-20 22:39:08 +0100

    Validators: add a task_exists validator

--- yatdlm/todo/helpers/routes_validators.py
+++ yatdlm/todo/helpers/routes_validators.py
@@ -1,5 +1,17 @@
 from functools import wraps
 from todo.models import Task, NotOwner
+from django.http import JsonResponse
+
+def task_exists(view):
+    @wraps(view)
+    def task_does_exists(request, list_id, task_id, *args, **kwargs):
+        try:
+            task = Task.objects.get(id=task_id, parent_list_id=list_id)
+            request.task = task
+            return view(request, list_id, task_id, *args, **kwargs)
+        except Task.DoesNotExist:
+            return JsonResponse({"errors": "The given task does not exists."}, status=404)
+    return task_does_exists
 
 def task_ownership(view):
     @wraps(view)
