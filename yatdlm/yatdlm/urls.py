a71fef20afba67a5760996b9c3abff71a50e0803
Kaci Adjou <adjou.kaci@gmail.com>
2022-05-15 22:29:29 +0200

    urls: add missed configuration import
    This commit adds a missed import that allows the app to add urls from
    Django Debug Toolbar's url set if the ``DEBUG`` configuration variable
    is set to ``True``.

--- yatdlm/yatdlm/urls.py
+++ yatdlm/yatdlm/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
+from django.conf import settings
 
 from todo import views as todo_views
 
