f2480fdb3b077ab1a207845826ec26193504e4c0
Kaci Adjou <adjou.kaci@gmail.com>
2023-02-19 09:07:51 +0100

    tests: rewrite deletion test case with factories
    This commit edits the current test case for todo list deletion by adding
    the usage of factories instead of forging the data "by hand" in the
    setup method.
    
    This commit also adds the usage of ``HTTPStatus`` instead of using bare
    ints in order to improve clarity on what is to be expected out of the
    API calls.

--- yatdlm/todo/tests/test_todolist.py
+++ yatdlm/todo/tests/test_todolist.py
@@ -1,40 +1,51 @@
+from http import HTTPStatus
+
+from django.contrib.auth.hashers import make_password
 from django.test import TestCase
-from django.contrib.auth import models as auth_models
+
+from todo.factories import TodoListFactory, UserFactory
 from todo.models import TodoList
 
-class TodoListTestCase(TestCase):
+
+class TodoListDelete(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = auth_models.User.objects.create_user('test', password='1234')
-        cls.other_user = auth_models.User.objects.create_user('test2', password='1234')
-        cls.list_ = TodoList(owner=cls.user)
-        cls.list_.save()
-        cls.url = '/todo/lists/delete/{}'
+        cls.users_password = "1234"
+        cls.user = UserFactory.create(
+            username="test", password=make_password(cls.users_password)
+        )
+        cls.other_user = UserFactory.create(
+            username="test2", password=make_password(cls.users_password)
+        )
+        cls.list_ = TodoListFactory.create(owner=cls.user)
+        cls.url = "/todo/lists/delete/{}"
 
     def test_delete_list(self):
-        list_ = TodoList(owner=self.user)
-        list_.save()
-        self.client.login(username='test', password='1234')
-        response = self.client.delete(self.url.format(list_.id))
-        self.assertEqual(response.status_code, 204)
-        with self.assertRaises(TodoList.DoesNotExist):
-            TodoList.objects.get(id=list_.id)
+        self.client.login(username="test", password=self.users_password)
+        response = self.client.delete(self.url.format(self.list_.id))
+
+        self.assertEqual(response.status_code, HTTPStatus.NO_CONTENT)
+        self.assertEqual(TodoList.objects.filter(id=self.list_.id).exists(), False)
 
     def test_delete_list_wrong_user(self):
         list_ = TodoList(owner=self.other_user)
         list_.save()
-        self.client.login(username='test', password='1234')
+
+        self.client.login(username="test", password=self.users_password)
         response = self.client.delete(self.url.format(list_.id))
-        self.assertEqual(response.status_code, 403)
-        self.assertIsNotNone(TodoList.objects.get(id=list_.id))
+
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+        self.assertEqual(TodoList.objects.filter(id=list_.id).exists(), True)
 
     def test_delete_list_user_not_logged(self):
         response = self.client.delete(self.url.format(self.list_.id))
-        self.assertEqual(response.status_code, 302)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
 
     def test_delete_list_wrong_verb(self):
-        self.client.login(username='test', password='1234')
+        self.client.login(username="test", password="1234")
         response = self.client.post(self.url.format(self.list_.id))
+
         # Expect a Method not allowed
-        self.assertEqual(response.status_code, 405)
-        self.assertIsNotNone(TodoList.objects.get(id=self.list_.id))
+        self.assertEqual(response.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
+        self.assertEqual(TodoList.objects.filter(id=self.list_.id).exists(), True)
